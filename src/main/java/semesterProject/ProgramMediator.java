b7547ec1146ad3ad892fc434b76d11ea9986ebfc
Chukwukodinaka <285152@via.dk>
2019-05-27 10:48:56 +0200

    Last one, hopefully working

--- src/main/java/semesterProject/ProgramMediator.java
+++ src/main/java/semesterProject/ProgramMediator.java
@@ -1,12 +1,14 @@
-package application;
+package semesterProject;
 
 import java.util.ArrayList;
 
 /**
- * A class containing a list of player objects @ author Simon Emmanuel @ version
- * 1.0
+ * A class containing a list of player objects
+ * @ author Simon Emmanuel
+ * @ version 1.0
  */
-public class ProgramMediator {
+public class ProgramMediator
+{
 	private MatchesList matchesList;
 	private PlayerList playerList;
 	private PlayersAdapter playerAdapter;
@@ -15,7 +17,8 @@ public class ProgramMediator {
 	/**
 	 * No argument constructor initializing the instance variables
 	 */
-	public ProgramMediator() {
+	public ProgramMediator()
+	{ 
 		this.playerAdapter = new PlayersAdapter("players.bin");
 		this.matchesAdapter = new MatchesAdapter("matches.bin");
 		this.matchesList = matchesAdapter.loadAllMatches();
@@ -23,24 +26,24 @@ public class ProgramMediator {
 	}
 
 	/**
-	 * Adds a player to the Player list
-	 * 
-	 * @param name     the name to add to the new player object
-	 * @param number   the number to add to the new player object
+	 * Adds a player to the Player list 
+	 * @param name the name to add to the new player object
+	 * @param number the number to add to the new player object
 	 * @param position the position to add to the new player object
 	 */
-	public void addPlayer(String name, int number, char position) {
-		playerList.addPlayer(new Player(name, number, position));
-		
+	public void addPlayer(String name,int number, char position)
+	{
+		playerList.addPlayer(new Player(name,number,position));
+
 		playerAdapter.savePlayers(playerList);
 	}
 
 	/**
 	 * remove a player from the list
-	 * 
 	 * @param player the player to remove from the list
 	 */
-	public void removePlayer(Player player) {
+	public void removePlayer(Player player)
+	{
 		playerList.removePlayer(player);
 		
 		playerAdapter.savePlayers(playerList);
@@ -49,34 +52,46 @@ public class ProgramMediator {
 	/**
 	 * @return all player objects in player list
 	 */
-	public PlayerList getAllPlayers() {
+	public PlayerList getAllPlayers()
+	{
 		return playerList;
 	}
 
+
 	/**
 	 * Gets a player with String name from the List
-	 * 
 	 * @param name the name in the list of the Player Object
 	 * @return the player with name if one exists, else return null
 	 */
-	public Player getPlayerByName(String name) {
-		return playerList.getPlayerByName(name);
+	public Player getPlayerByName(String name)
+	{
+		for (int i = 0; i < playerList.getNumberOfPlayers(); i++)
+		{
+			if (playerList.getPlayer(i).getName().equals(name))
+				return playerList.getPlayer(i);
+		}
+
+		return null;
 	}
 
 	/**
-	 * Gets player objects by char position from the List
-	 * 
+	 * Gets  player objects by char position from the List
 	 * @param position the position in the list of the Player Object
 	 * @return the players at position if one exists, else return null
 	 */
-	public ArrayList<Player> getPlayersByPosition(char position) {
-		return playerList.getPlayersByPosition(position);
-	}
-
+	public PlayerList getPlayersByPosition(char position) {
+		PlayerList temp = new PlayerList();
+		for (int i = 0; i < playerList.getNumberOfPlayers(); i++) {
+			if (playerList.getPlayer(i).getPosition() == position)
+				temp.addPlayer(playerList.getPlayer(i));
+		}
+		return temp;
+	} 
+	
 	public void addPlayer(Player player) {		
 		playerList.addPlayer(player);
 
 		playerAdapter.savePlayers(playerList);
 	}
-
+	
 }
