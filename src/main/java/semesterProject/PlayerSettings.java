26825a6e7b755ee7137e50af017dce2692dc01b1
Rafal Pierscieniak <279471@via.dk>
2019-05-27 23:51:31 +0200

    new controller PlayerSettings.java

--- src/main/java/semesterProject/PlayerSettings.java
+++ src/main/java/semesterProject/PlayerSettings.java
@@ -1,4 +1,27 @@
 package semesterProject;
 
-public class PlayerSettings {
+import java.io.IOException;
+import java.util.List;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+
+public class PlayerSettings{
+    @FXML private DatePicker date;
+    @FXML private TextField opponent;
+    @FXML private ComboBox place;
+    @FXML private ComboBox type;
+
+
 }
