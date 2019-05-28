3e10cc71537c767de31bc0d1bd2f6ad82224bc7d
Rafal Pierscieniak <279471@via.dk>
2019-05-28 02:37:59 +0200

    Controler MatchSettings adding

--- src/main/java/semesterProject/PlayerSettings.java
+++ src/main/java/semesterProject/PlayerSettings.java
@@ -1,27 +1,6 @@
 package semesterProject;
 
-import java.io.IOException;
-import java.util.List;
-
-import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Stage;
-
-
 public class PlayerSettings{
-    @FXML private DatePicker date;
-    @FXML private TextField opponent;
-    @FXML private ComboBox place;
-    @FXML private ComboBox type;
 
 
 }
